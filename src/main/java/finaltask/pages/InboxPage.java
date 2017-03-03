300251235e699d803502f63a4612cf544ed548ab
Oleksandr Pochernin <Oleksandr_Pochernin@epam.com>
2017-03-03 16:53:38 +0200

    Bug fix

--- src/main/java/finaltask/pages/InboxPage.java
+++ src/main/java/finaltask/pages/InboxPage.java
@@ -11,9 +11,9 @@ public class InboxPage extends BasePage {
 
     private static final String WRITE_ELEMENT_LETTER_BUTTON = "//div[@role='navigation']/preceding-sibling:: div//div[@role='button']";
     private static final String NEW_LETTER_LINK = "(//div[@role='navigation']//a)[1]";
-    private static final String ALL_LETTER_CHECKBOX = "//span[@role='checkbox']/div";
-    private static final String DELETE_LETTER_BUTTON = "//div/div[@role='button' and @tabindex='0'][3]/div/div";
     private static final String MESSAGE_SUBJECT_LIST = "//div[@role='link']//b";
+    private static final String ALL_LETTER_CHECKBOX = "//span[@role='checkbox']/div";
+    private static final String DELETE_LETTER_BUTTON = "//div/div[@role='button' and @tabindex='0'][3]/div";
 
     @FindBy(xpath = WRITE_ELEMENT_LETTER_BUTTON)
     private WebElement writeLetterButton;
