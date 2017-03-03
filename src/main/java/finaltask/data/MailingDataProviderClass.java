39b28ae77018ab18b84d68616a0ae1d375b8eeff
Oleksandr Pochernin <Oleksandr_Pochernin@epam.com>
2017-03-03 18:58:14 +0200

    Dataprovider refactor

--- src/main/java/finaltask/data/MailingDataProviderClass.java
+++ src/main/java/finaltask/data/MailingDataProviderClass.java
@@ -16,11 +16,4 @@ public class MailingDataProviderClass {
                 {GMAIL_LOGIN_EMAIL, GMAIL_LOGIN_PSS, GMAIL_TO_EMAIL, GMAIL_MESSAGE_SUBJECT, GMAIL_MESSAGE_TEXT, SUBJECT_TEXT}
         };
     }
-
-    @DataProvider(name = "DeleteInboxLetter")
-    public static Object[][] deletingQuery() {
-        return new Object[][]{
-                {GMAIL_LOGIN_EMAIL, GMAIL_LOGIN_PSS}
-        };
-    }
 }
