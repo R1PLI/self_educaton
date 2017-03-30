90191bd65dc140a0ddfea91a8db443ae7be0d469
Oleksandr Pochernin <oleksandr_pochernin@epam.com>
2017-03-30 17:16:48 +0300

    Minor changes

--- src/main/java/utils/helper/MathHelper.java
+++ src/main/java/utils/helper/MathHelper.java
@@ -92,7 +92,7 @@ public class MathHelper {
     }
 
     public static Integer minValueInList() {
-        return Stream.of(3, 2, 1)
+        return Stream.of(1, 2, 3)
                 .min(Integer::compareTo)
                 .get();
     }
